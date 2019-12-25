f47b32992d043f40cb7de484620ac243e417ad8c
MohammedAyman2018 <emidobibo20@gmail.com>
2019-12-25 09:36:22 +0200

    Fix Navbar & Fix cards

--- js/main.js
+++ js/main.js
@@ -1,41 +1,18 @@
 window.onload = function () {
-    // Animate();
+    Animate();
 }
 
 // Make Navbar interactive
-var navbar = document.getElementById('Nav'),
-    toogle = document.querySelector('nav button.navbar-toggler i');
-
-window.onscroll = function () {
-    if (window.scrollY <= 100) {
-        move(navbar)
-            .set('background', 'transparent')
-            .duration('0.7s')
-            .end();
-
-        move(toogle)
-            .set("color", "white")
-            .end()
-    } else {
-        move(navbar)
-            .set('background', '#E7E8EA')
-            .duration('0.7s')
-            .end();
-
-        move(toogle)
-            .set("color", "black")
-            .end()
-    }
-};
+$(window).scroll(function(){
+	$('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
+});
 
 function Animate() {
-    move('.overlay')
-        .x(0)
-        .duration(3000)
-        .then()
-    move('.preload')
-        .x(4000)
-        .duration(2000)
-        .delay(1000)
-        .end();
+    $('.preload').animate({width: '0'}, 
+    {
+        duration: 500,
+        complete: function () {
+           $(this).css({"display" : "none"})
+        },
+    });
 }
